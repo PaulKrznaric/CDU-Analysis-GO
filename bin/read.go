3bf2e67cc76336d90c86271b167ca7d551d0cf71
PaulKrznaric <paulkrznaric@gmail.com>
2022-06-11 22:09:35 -0400

    Partially completed parsing of data

--- bin/read.go
+++ bin/read.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strconv"
+	"time"
 
 	"github.com/xuri/excelize/v2"
 )
@@ -13,22 +16,45 @@ func readFile() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Opened sheet")
 
 	//read the file line by line
-	for i := 1; i <= file.SheetCount; i++ {
-		sheet := file.GetSheetName(i)
-		rows, err := file.GetRows(sheet)
-		if err != nil {
-			log.Fatal(err)
+	sheet := file.GetSheetName(0)
+	rows, err := file.GetRows(sheet, excelize.Options{RawCellValue: true})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, row := range rows {
+		if row[0] == "MRN" {
+			continue
 		}
-		for _, row := range rows {
-			for _, cell := range row {
-				log.Print(cell)
-			}
+		id := row[0]
+		timeIn := createTime(row[2])
+		primaryDoctor := row[3]
+		secondaryDoctor := row[7]
+		if secondaryDoctor == "." {
+			secondaryDoctor = ""
+		}
+		if len(row) != 13 {
+			fmt.Println(len(row))
+			fmt.Println(id, " ", timeIn, " ", primaryDoctor, " ", secondaryDoctor)
 		}
 	}
-
 	//close the file
 	defer file.Close()
 
+}
+
+func createTime(timeString string) *time.Time {
+	if timeString == "" {
+		return nil
+	}
+	//convert the string to an int
+	timeInt, err := strconv.ParseFloat(timeString, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	excelEpoch := time.Date(1899, 12, 30, 0, 0, 0, 0, time.UTC)
+	time := excelEpoch.Add(time.Duration(timeInt * float64(24*time.Hour)))
+	return &time
 }
